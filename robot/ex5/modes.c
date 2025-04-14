c08469cc1b4e73aedb0bb8011c04ef3d354ebbdd
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2025-04-14 08:07:32 +0200

    Add ex5.1

--- robot/ex5/modes.c
+++ robot/ex5/modes.c
@@ -6,6 +6,7 @@
 #include "hardware.h"
 
 const float FREQ = 1.0;   // Hz
+const uint8_t MOTOR_ADDR = 21;
 
 void sine_demo_mode()
 {
@@ -43,6 +44,44 @@ void sine_demo_mode()
   set_color(2);
 }
 
+void sine_motor_mode()
+{
+  init_body_module(MOTOR_ADDR);
+  start_pid(MOTOR_ADDR);
+  set_color(4);
+
+  uint32_t dt, cycletimer;
+  float my_time, delta_t, l;
+  int8_t l_rounded;
+
+  cycletimer = getSysTICs();
+  my_time = 0;
+
+  do {
+    // Calculates the delta_t in seconds and adds it to the current time
+    dt = getElapsedSysTICs(cycletimer);
+    cycletimer = getSysTICs();
+    delta_t = (float) dt / sysTICSperSEC;
+    my_time += delta_t;
+
+    // Calculates the sine wave
+    l = 32 * sin(M_TWOPI * FREQ * my_time);
+
+    bus_set(MOTOR_ADDR, MREG_SETPOINT, DEG_TO_OUTPUT_BODY(l));
+
+    // Make sure there is some delay, so that the timer output is not zero
+    pause(ONE_MS);
+
+  } while (reg8_table[REG8_MODE] == IMODE_SINE_DEMO);
+
+  bus_set(MOTOR_ADDR, MREG_SETPOINT, DEG_TO_OUTPUT_BODY(0.0));
+  pause(ONE_SEC);
+  bus_set(MOTOR_ADDR, MREG_MODE, MODE_IDLE);
+
+  // Back to the "normal" green
+  set_color(2);
+}
+
 void main_mode_loop()
 {
   reg8_table[REG8_MODE] = IMODE_IDLE;
@@ -55,6 +94,9 @@ void main_mode_loop()
         break;
       case IMODE_SINE_DEMO:
         sine_demo_mode();
+        break;
+      case IMODE_MOTOR_DEMO:
+        sine_motor_mode();
         break;
       default:
         reg8_table[REG8_MODE] = IMODE_IDLE;
