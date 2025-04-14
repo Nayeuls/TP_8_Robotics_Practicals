91294b0ee5c27897d14b07241eb0544482bf8986
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2025-04-14 13:39:42 +0200

    Fix ex5

---
+++ pc/ex5/ex5.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "remregs.h"
+#include "robot.h"
+#include "utils.h"
+#include "regdefs.h"
+#include "../../robot/ex4/modes.h"
+#include <math.h>
+
+const uint8_t RADIO_CHANNEL = 201;         ///< robot radio channel
+const char* INTERFACE = "COM1";            ///< robot radio interface
+
+int main()
+{
+    CRemoteRegs regs;
+
+    if (!init_radio_interface(INTERFACE, RADIO_CHANNEL, regs)) {
+        return 1;
+    }
+
+    // Reboots the head microcontroller to make sure it is always in the same state
+    reboot_head(regs);
+  
+    regs.set_reg_b(REG8_MODE, IMODE_IDLE); // Set mode to idle
+    while (!kbhit()){
+        // Mode demo
+        regs.set_reg_b(REG8_MODE, IMODE_MOTOR_DEMO); // Set mode to motor demo
+    }
+    ext_key();
+    while (!kbhit()){
+        // Mode sin
+        regs.set_reg_b(REG8_MODE, IMODE_MOTOR_SIN); // Set mode to sin demo
+    }
+    
+    regs.set_reg_b(REG8_MODE,0); // Set mode to idle
+    regs.close();
+    return 0;
+}
--- robot/ex5/modes.c
+++ robot/ex5/modes.c
@@ -72,7 +72,7 @@ void sine_motor_mode()
     // Make sure there is some delay, so that the timer output is not zero
     pause(ONE_MS);
 
-  } while (reg8_table[REG8_MODE] == IMODE_SINE_DEMO);
+  } while (reg8_table[REG8_MODE] == IMODE_MOTOR_DEMO);
 
   bus_set(MOTOR_ADDR, MREG_SETPOINT, DEG_TO_OUTPUT_BODY(0.0));
   pause(ONE_SEC);
