b22a034f121390b03183eca754439397c8e0d338
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2025-04-28 14:09:13 +0200

    Add retransmition

--- pc/ex7/ex7.cc
+++ pc/ex7/ex7.cc
@@ -20,6 +20,17 @@ const uint16_t TRACKING_PORT = 10502;          ///< port number of the tracking 
 const uint8_t RADIO_CHANNEL = 126;         ///< robot radio channel
 const char* INTERFACE = "COM3";            ///< robot radio interface
 
+bool set_reg_b(CRemoteRegs& regs, const uint16_t addr, const uint8_t val){
+  for(int i=0; i<10; i++){
+    if (regs.set_reg_b(addr, val)) {
+      return true;
+    }
+    Sleep(1);
+  }
+  std::cerr << "Unable to set register " << addr << " to " << (int) val << endl;
+  return false;
+}
+
 int main()
 {
   CTrackingClient trk;
@@ -48,14 +59,14 @@ int main()
   std::cin >> amplitude;
   std::cout << "Enter phase lag: ";
   std::cin >> phase_lag;
-  regs.set_reg_b(REG8_FREQ, ENCODE_PARAM_8(freq, 0.0, FREQ_MAX));
-  regs.set_reg_b(REG8_AMPLITUDE, ENCODE_PARAM_8(0, 0.0, AMPLITUDE_MAX));
-  regs.set_reg_b(REG8_PHASE_LAG, ENCODE_PARAM_8(phase_lag, 0.5, 1.5));
+  set_reg_b(regs, REG8_FREQ, ENCODE_PARAM_8(freq, 0.0, FREQ_MAX));
+  set_reg_b(regs, REG8_AMPLITUDE, ENCODE_PARAM_8(0, 0.0, AMPLITUDE_MAX));
+  set_reg_b(regs, REG8_PHASE_LAG, ENCODE_PARAM_8(phase_lag, 0.5, 1.5));
 
 
   std::cout << "Presser une touche pour commencer: ";
   ext_key();
-  regs.set_reg_b(REG8_AMPLITUDE, ENCODE_PARAM_8(amplitude, 0.0, AMPLITUDE_MAX));
+  set_reg_b(regs, REG8_AMPLITUDE, ENCODE_PARAM_8(amplitude, 0.0, AMPLITUDE_MAX));
 
   string file_name = string("Amplitude_") + std::to_string(amplitude) + "_frequency_" + std::to_string(freq);
 
@@ -94,7 +105,7 @@ int main()
     Sleep(10);
   }
 
-  regs.set_reg_b(REG8_AMPLITUDE, ENCODE_PARAM_8(0, 0.0, AMPLITUDE_MAX));
+  set_regs(regs, REG8_AMPLITUDE, ENCODE_PARAM_8(0, 0.0, AMPLITUDE_MAX));
   
   // Clears the console input buffer (as kbhit() doesn't)
   FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
