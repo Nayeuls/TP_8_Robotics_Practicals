b792eba749255923b3b7dcec159cd2a6d9fc5ae6
LTronchet <loic.tronchet@epfl.ch>
2025-05-04 16:21:59 +0200

    Clean up main exercise 4

--- pc/ex4/ex4.cc
+++ pc/ex4/ex4.cc
@@ -21,18 +21,13 @@ int main()
     reboot_head(regs);
   
     regs.set_reg_b(REG8_MODE, IMODE_IDLE); // Set mode to idle
-    while (!kbhit()){
-        // Mode demo
-        regs.set_reg_b(REG8_MODE, IMODE_MOTOR_DEMO); // Set mode to motor demo
-    }
+    regs.set_reg_b(REG8_MODE, IMODE_MOTOR_DEMO); // Set mode to motor demo
     ext_key();
+    regs.set_reg_b(REG8_MODE, IMODE_MOTOR_SIN); // Set mode to motor sin
     while (!kbhit()){
-        // Mode sin
-        regs.set_reg_b(REG8_MODE, IMODE_MOTOR_SIN); // Set mode to sin demo
         regs.set_reg_b(2, 40*(sin(2*M_PI*time_d())));  // sine wave at 1 Hz (takes rad)
     }
-    
-    regs.set_reg_b(REG8_MODE,0); // Set mode to idle
+    regs.set_reg_b(REG8_MODE, IMODE_IDLE); // Set mode to idle
     regs.close();
     return 0;
 }
